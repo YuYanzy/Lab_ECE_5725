0024b4c5851788d6a9d6ff92230b23dfd289ce8a
Steven Zhang <yz2729@cornell.com>
2021-10-21 02:44:42 -0400

    upload some results for week2

---
+++ code/lab4_code/sort_v1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# chmod +x sort_v1.sh
+
+./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 &
+./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 &
+./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 &
+./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 &
+./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 & ./sort_v1 &
+
