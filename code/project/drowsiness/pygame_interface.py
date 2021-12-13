45a9f564b266ebaaa07c212a042120456ac1954e
Steven Zhang <913835211@qq.com>
2021-12-13 10:51:45 -0500

    change interface

--- code/project/drowsiness/pygame_interface.py
+++ code/project/drowsiness/pygame_interface.py
@@ -6,7 +6,6 @@ import time
 import RPi.GPIO as GPIO
 
 # set up the enviroments
-# os.putenv('SDL_VIDEODRIVER', 'x11') # Display on piTFT
 # os.putenv('SDL_VIDEODRIVER', 'fbcon') # Display on piTFT
 # os.putenv('SDL_FBDEV', '/dev/fb1')
 # os.putenv('SDL_MOUSEDRV', 'TSLIB') # Track mouse clicks on piTFT
@@ -35,8 +34,7 @@ def GPIO27_callback(channel):
     
     
 pygame.init()
-# screen = pygame.display.set_mode((800,600))
-size = (width, height) = (600, 800)
+size = (width, height) = (320, 240)
 screen = pygame.display.set_mode(size)
 my_font = pygame.font.Font(None,26)
 white = 255,255,255
@@ -46,34 +44,49 @@ red = 255,0,0
 
 
 drowsy_alert = my_font.render("Drowsy", True, white)
-rect_drowsy_alert = drowsy_alert.get_rect(center = (150,350))
+rect_drowsy_alert = drowsy_alert.get_rect(center = (80,90))
 distracted_alert = my_font.render("Looking Away", True, white)
-rect_distracted_alert = distracted_alert.get_rect(center = (450,350))
+rect_distracted_alert = distracted_alert.get_rect(center = (240,90))
 yawn_alert = my_font.render("Yawn", True, white)
-rect_yawn_alert = yawn_alert.get_rect(center = (150, 650))
+rect_yawn_alert = yawn_alert.get_rect(center = (80, 190))
 wheel_alert = my_font.render("Wheel", True, white)
-rect_wheel_alert = wheel_alert.get_rect(center = (450,650))
+rect_wheel_alert = wheel_alert.get_rect(center = (240,190))
 
+drowsy_counter = 0
+distracted_counter = 0
+yawn_counter = 0
+wheel_counter = 0
+
+speed_info = "Can not detecting the speed"
+info_font = pygame.font.Font(None,20)
 
 def draw_interface():
-
-    # pygame.draw.line(screen, white, [0,0], [319,0], 3)
-    # pygame.draw.line(screen, white, (319,0), (319,240), 3)
-    # pygame.draw.line(screen, white, (0,238), (320,238), 3)
-    # pygame.draw.line(screen, white, (0,0), (0,240), 3)
-
-    # pygame.draw.line(screen, white, (160,0), (160,240), 3)
-    # pygame.draw.line(screen, white, (0,120), (320,120), 3)
+    global speed_info, drowsy_counter, distracted_counter, yawn_counter, wheel_counter
+    speed_surface = info_font.render(speed_info,False,white)
+    screen.blit(speed_surface,(160,20))
+    drowsy_counter_surface = info_font.render("Drowsy: " + str(drowsy_counter),False,white)
+    screen.blit(drowsy_counter_surface,(140,70))
+    distracted_counter_surface = info_font.render("Distracted: " + str(distracted_counter),False,white)
+    screen.blit(distracted_counter_surface,(300,70))
+    yawn_counter_surface = info_font.render("Yawn: " + str(yawn_counter),False,white)
+    screen.blit(yawn_counter_surface,(140,220))
+    wheel_counter_surface = info_font.render("Wheel: " + str(wheel_counter),False,white)
+    screen.blit(wheel_counter_surface,(300,220))
     
-    pygame.draw.line(screen, white, [0, 200], [600, 200], 3)
-    pygame.draw.line(screen, white, [0, 500], [600, 500], 3)
-    pygame.draw.line(screen, white, [300, 200], [300, 800], 3)
+    pygame.draw.line(screen, white, [0,0], [320,0], 3)
+    pygame.draw.line(screen, white, (320,0), (320,240), 3)
+    pygame.draw.line(screen, white, (0,240), (320,240), 3)
+    pygame.draw.line(screen, white, (0,0), (0,240), 3)
+    
+    pygame.draw.line(screen, white, [0, 40], [320, 40], 3)
+    pygame.draw.line(screen, white, [0, 140], [320, 140], 3)
+    pygame.draw.line(screen, white, [160, 40], [160, 200], 3)
     pygame.display.flip()
 
 
 def update_interface():
     global CODERUN
-
+    global speed_info, drowsy_counter, distracted_counter, yawn_counter, wheel_counter
     while CODERUN:
         # time.sleep(0.25)
         current_line = speed_file.readline()
@@ -81,47 +94,52 @@ def update_interface():
         current_speed = 0
         if current_line != "":
             current_speed = float(current_line.split( )[1])
+            speed_info = "Current speed: " + str(current_speed) + " mph"
         f = open('message.txt', 'r')
         lines = f.readlines()
         if current_speed > 0:
             if len(lines) > 0:
                 last_line = lines[-1]
-                print(last_line)
+                print(last_line) 
                 cmd = ""
                 # First Quadrant
                 if last_line == "DROWSY\n":
-                    pygame.draw.rect(screen,red,(0,200,300,300))
+                    drowsy_counter += 1
+                    pygame.draw.rect(screen,red,(0,40,80,50))
                     screen.blit(drowsy_alert, rect_drowsy_alert)
                     cmd = 'espeak -ven+f2 -k5 -s150 --stdout  "Drowsing" | aplay '
                     
                 if last_line == "NOT_DROWSY\n":
-                    pygame.draw.rect(screen,black,(0,200,300,300))
+                    pygame.draw.rect(screen,black,(0,40,80,50))
                 
                 # Second Quadrant
                 if last_line == "EYE_OFF_ROAD\n":
-                    pygame.draw.rect(screen,red,(300,200,300,300))
+                    distracted_counter += 1
+                    pygame.draw.rect(screen,red,(160,40,80,50))
                     screen.blit(distracted_alert, rect_distracted_alert)
-                    pygame.draw.rect(screen,black,(0,200,300,300))
-                    pygame.draw.rect(screen,black,(0,500,300,300))
+                    pygame.draw.rect(screen,black,(0,40,80,50))
+                    pygame.draw.rect(screen,black,(0,140,80,50))
                     cmd = 'espeak -ven+f2 -k5 -s150 --stdout  "Distracted" | aplay '
                 if last_line == "EYE_ON_ROAD\n":
-                    pygame.draw.rect(screen,black,(300,200,300,300))
+                    pygame.draw.rect(screen,black,(160,40,80,50))
 
                 # Third Quadrant
                 if last_line == "YAWN\n":
-                    pygame.draw.rect(screen,red,(0,500,300,300))
+                    yawn_counter += 1
+                    pygame.draw.rect(screen,red,(0,140,80,50))
                     screen.blit(yawn_alert, rect_yawn_alert)
                     cmd = 'espeak -ven+f2 -k5 -s150 --stdout  "Yawning" | aplay '
                 if last_line == "NOT_YAWN\n":
-                    pygame.draw.rect(screen,black,(0,500,300,300))
+                    pygame.draw.rect(screen,black,(0,140,80,50))
                 
                 # Last Quadrant
                 if last_line == "HAND_OFF_WHEEL\n":
-                    pygame.draw.rect(screen,red,(300,500,300,300))
+                    wheel_counter += 1
+                    pygame.draw.rect(screen,red,(160, 90,80,50))
                     screen.blit(wheel_alert,rect_wheel_alert)
                     cmd = 'espeak -ven+f2 -k5 -s150 --stdout  "Hand off Wheel" | aplay '
                 if last_line == "HAND_ON_WHEEL\n":
-                    pygame.draw.rect(screen,black,(300,500,300,300))
+                    pygame.draw.rect(screen,black,(160, 90,80,50))
                 
                 if cmd != "":
                     os.system(cmd)
